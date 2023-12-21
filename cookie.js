609dba3046a3b89d1ab19126aaf6066b7a7c763c
djkeu <mrkooij@gmail.com>
2023-12-21 14:33:24 +0100

    Create writeCookie()

---
+++ cookie.js
@@ -0,0 +1,16 @@
+function writeCookie(name, value, days) {
+    // By default the cookie is temporary
+    var expires = "";
+
+    // Specify number of days cookie should be persistant
+    if (days) {
+        var date = new Date();
+        // Convert days to milliseconds, add to current time
+        date,setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+
+    // Set the cookie to the name, value and expiration date
+    document.cookie = name + "=" + value + expires + "path=/";
+}
+
